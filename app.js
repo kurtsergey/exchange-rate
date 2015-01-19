63a330a4e7b05c4c3fc0cf3c42e6e66142bcf91e
kurtsergey <kurtsergey@gmail.com>
2015-01-19 17:32:23 +0300

    + selectors storage

--- app.js
+++ app.js
@@ -1,43 +1,182 @@
 ﻿
 $(function ()
 {
+    var procId;
+
+    procId = setTimeout(findSelectors, 1000);
+
+    $(document).bind("DOMSubtreeModified", function ()
+    {
+        clearTimeout(procId);
+        procId = setTimeout(findSelectors, 1000);
+    });
+
+
+
     $(document).on('click', function (e)
     {
         if (e.originalEvent.ctrlKey || e.originalEvent.metaKey)
         {
             var t = $(e.target);
-
-            var text_nodes = t.contents().filter(function ()
+            if (t.data('exchange-rate-originalHtml'))
             {
-                return this.nodeType == Node.TEXT_NODE;
-            });
-
-            if (text_nodes.length > 0)
+                var s = getSelector(t);
+                replacePricesToOriginal(s);
+                removeSelector(s);
+            }
+            else
             {
-                var original_content = t.clone();
-
-                text_nodes.replaceWith(function (i)
+                var text_nodes = t.contents().filter(function ()
                 {
-                    return $(this).text().replace(/([\d\s]*)/g, "<number>$1</number>");
+                    return this.nodeType == Node.TEXT_NODE;
                 });
 
-                var hit_word_elem = document.elementFromPoint(e.clientX, e.clientY);
-
-                if (hit_word_elem.nodeName == 'NUMBER')
+                if (text_nodes.length > 0)
                 {
-                    hit_word = $(hit_word_elem).text();
+                    var original_content = t.clone();
 
-                    chrome.runtime.sendMessage({ type: 'parsed', text: hit_word }, function (response)
+                    text_nodes.replaceWith(function (i)
                     {
+                        return $(this).text().replace(/([\d\s.,]*)/g, "<number>$1</number>");
                     });
-                }
 
-                t.replaceWith(original_content);
+                    var hit_word_elem = document.elementFromPoint(e.clientX, e.clientY);
+
+                    if (hit_word_elem.nodeName == 'NUMBER')
+                    {
+                        hit_word = $(hit_word_elem).text();
+
+                        chrome.runtime.sendMessage({ type: 'parsed', text: hit_word }, function (response) { });
+
+                        if (t.text() == hit_word)
+                        {
+                            var s = getSelector(t);
+                            replacePrices(s);
+                            pushSelector(s);
+                        }
+                    }
+
+                    t.replaceWith(original_content);
+                }
             }
 
             e.preventDefault();
         }
     });
+
+
+
+    function getSelector($node)
+    {
+        return $node.parents().map(function (index, o) { return o.tagName + ' > '; }).toArray().reverse().join(' ') + ' ' + $node.prop("tagName");
+    }
+
+
+    function replacePrices(selector)
+    {
+        chrome.storage.sync.get({
+            defaultCurrency: 'usd'
+        }, function (sync)
+        {
+            $(selector).each(function (index, item)
+            {
+                var $item = $(item);
+                chrome.runtime.sendMessage({ type: 'found', text: $item.text(), curr: sync.defaultCurrency }, function (response)
+                {
+                    $item.data('exchange-rate-originalHtml', $item.html());
+                    $item.html(response.text + ' <span class="exchange-rate-icon ' + (sync.defaultCurrency || '').toLowerCase() + '"></span>');
+                });
+
+            });
+        });
+    }
+
+    function replacePricesToOriginal(selector)
+    {
+        $(selector).each(function (index, item)
+        {
+            var $item = $(item);
+            $item.html($item.data('exchange-rate-originalHtml'));
+            $item.removeData('exchange-rate-originalHtml')
+        });
+    }
+
+
+
+
+
+    function findSelectors()
+    {
+        var o = {};
+        var hn = 'selectors-' + document.location.hostname;
+        o[hn] = '';
+
+
+        chrome.storage.sync.get(o, function (sync)
+        {
+            if (!!sync[hn])
+            {
+                var items = sync[hn].split(',');
+                for (var i = 0; i < items.length; ++i)
+                {
+                    replacePrices(items[i]);
+                }
+            }
+        });
+    }
+
+
+
+
+
+    function pushSelector(selector)
+    {
+        var o = {};
+        var hn = 'selectors-' + document.location.hostname;
+        o[hn] = '';
+
+        chrome.storage.sync.get(o, function (sync)
+        {
+            if (!sync[hn])
+            {
+                sync[hn] = selector;
+            }
+            else
+            {
+                var items = sync[hn].split(',');
+                if (items.indexOf(selector) == -1)
+                {
+                    items.push(selector);
+                }
+                sync[hn] = items.join(',');
+            }
+            chrome.storage.sync.set(sync, function () { });
+        });
+    }
+
+    function removeSelector(selector)
+    {
+        var o = {};
+        var hn = 'selectors-' + document.location.hostname;
+        o[hn] = '';
+
+        chrome.storage.sync.get(o, function (sync)
+        {
+            if (!!sync[hn])
+            {
+                var items = sync[hn].split(',');
+
+                var index = items.indexOf(selector);
+                if (index !== -1)
+                {
+                    items.splice(index, 1);
+                }
+                sync[hn] = items.join(',');
+            }
+            chrome.storage.sync.set(sync, function () { });
+        });
+    }
+
 
 
     //if (document.location.hostname == 'ab.onliner.by')
