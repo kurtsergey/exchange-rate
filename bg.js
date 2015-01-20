acb15954ba6d93d83b2fbb2dc846ecc4b3e28af5
kurtsergey <kurtsergey@gmail.com>
2015-01-20 14:34:57 +0300

    * rates are taking one time per day

--- bg.js
+++ bg.js
@@ -1,192 +1,207 @@
-﻿
-var byr,
-    usd,
-    eur,
-    rub,
-
-    usdRate,
-    eurRate,
-    rubRate;
-
-
-
-chrome.storage.sync.get({
-    usdRate: 0,
-    eurRate: 0,
-    rubRate: 0
-}, function (items)
+﻿(function ()
 {
-    usdRate = items.usdRate;
-    eurRate = items.eurRate;
-    rubRate = items.rubRate;
-});
+    var byr,
+        usd,
+        eur,
+        rub,
+
+        usdRate,
+        eurRate,
+        rubRate;
 
 
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse)
-{
-    var t, i;
-
-    if (request.type == 'parsed')
+    chrome.storage.sync.get({
+        usdRate: 0,
+        eurRate: 0,
+        rubRate: 0
+    }, function (items)
     {
-        t = request.text.split(/\s*/).join('');
-
-        try
-        {
-            i = parseFloat(t);
-        } catch (ex) { };
-
-        if (i && !isNaN(i))
-        {
-            byr = i;
-
-            if (usdRate && !isNaN(usdRate))
-            {
-                usd = i / usdRate;
-            }
-
-            if (eurRate && !isNaN(eurRate))
-            {
-                eur = i / eurRate;
-            }
-
-            if (rubRate && !isNaN(rubRate))
-            {
-                rub = i / rubRate;
-            }
+        usdRate = items.usdRate;
+        eurRate = items.eurRate;
+        rubRate = items.rubRate;
+    });
 
 
-            chrome.storage.sync.get({
-                notification: true,
-                alert: false,
-                defaultCurrency: 'usd'
-            }, function (items)
-            {
-                var title,
-                    message;
 
-                if (items.defaultCurrency == 'usd')
-                {
-                    title = '$' + numberToString(usd);
-                    message = '€' + numberToString(eur) + '\n' + '₽' + numberToString(rub);
-                }
-                else if (items.defaultCurrency == 'eur')
-                {
-                    title = '€' + numberToString(eur);
-                    message = '$' + numberToString(usd) + '\n' + '₽' + numberToString(rub);
-                }
-                else
-                {
-                    title = '₽' + numberToString(rub);
-                    message = '$' + numberToString(usd) + '\n' + '€' + numberToString(eur);
-                }
-
-                message += '\n' + numberToString(i) + ' BYR';
-
-                if (items.notification)
-                {
-                    chrome.notifications.create('', { type: 'basic', title: title, message: message, iconUrl: '128.png' }, function (n)
-                    {
-                        setTimeout(function ()
-                        {
-                            chrome.notifications.clear(n, function callback()
-                            {
-                            })
-                        }, 3000);
-                    });
-                }
-
-                if (items.alert)
-                {
-                    alert(title);
-                }
-            });
-
-        }
-    }
-    else if (request.type == 'found')
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse)
     {
+        var t, i;
 
-        t = request.text.split(/\s*/).join('');
-
-        try
+        if (request.type == 'parsed')
         {
-            i = parseFloat(t);
-        } catch (ex) { };
+            t = request.text.split(/\s*/).join('');
 
-        if (i && !isNaN(i))
-        {
-            var title;
-
-            if (request.curr == 'usd')
+            try
             {
+                i = parseFloat(t);
+            } catch (ex) { };
+
+            if (i && !isNaN(i))
+            {
+                byr = i;
+
                 if (usdRate && !isNaN(usdRate))
                 {
-                title = '$' + numberToString(i / usdRate);
+                    usd = i / usdRate;
                 }
-            }
-            else if (request.curr == 'eur')
-            {
+
                 if (eurRate && !isNaN(eurRate))
                 {
-                    title = '€' + numberToString(i / eurRate);
+                    eur = i / eurRate;
                 }
-            }
-            else if (request.curr == 'rub')
-            {
+
                 if (rubRate && !isNaN(rubRate))
                 {
-                    title = '₽' + numberToString(i / rubRate);
+                    rub = i / rubRate;
                 }
+
+
+                chrome.storage.sync.get({
+                    notification: true,
+                    alert: false,
+                    defaultCurrency: 'usd'
+                }, function (items)
+                {
+                    var title,
+                        message;
+
+                    if (items.defaultCurrency == 'usd')
+                    {
+                        title = '$' + numberToString(usd);
+                        message = '€' + numberToString(eur) + '\n' + '₽' + numberToString(rub);
+                    }
+                    else if (items.defaultCurrency == 'eur')
+                    {
+                        title = '€' + numberToString(eur);
+                        message = '$' + numberToString(usd) + '\n' + '₽' + numberToString(rub);
+                    }
+                    else
+                    {
+                        title = '₽' + numberToString(rub);
+                        message = '$' + numberToString(usd) + '\n' + '€' + numberToString(eur);
+                    }
+
+                    message += '\n' + numberToString(i) + ' BYR';
+
+                    if (items.notification)
+                    {
+                        chrome.notifications.create('', { type: 'basic', title: title, message: message, iconUrl: '128.png' }, function (n)
+                        {
+                            setTimeout(function ()
+                            {
+                                chrome.notifications.clear(n, function callback()
+                                {
+                                })
+                            }, 3000);
+                        });
+                    }
+
+                    if (items.alert)
+                    {
+                        alert(title);
+                    }
+                });
+
             }
-
-            sendResponse({ text: title });
-
         }
-    }
-    else if (request.type == 'rates')
-    {
-        sendResponse({
-            usdRate: usdRate,
-            eurRate: eurRate,
-            rubRate: rubRate,
-            byr: byr,
-            usd: usd,
-            eur: eur,
-            rub: rub
-        });
-    }
-});
-
-
-
-
-function take()
-{
-    $.ajax('http://www.nbrb.by/').always(function (data)
-    {
-        var $data = $(data);
-        var $table = $data.find('.content .tmainnn');
-
-        var td = $table.find('td:contains("USD")').next().next();
-        usdRate = +(td.text().split(/\s*/).join('').replace(',', '.'));
-
-        td = $table.find('td:contains("EUR")').next().next();
-        eurRate = +(td.text().split(/\s*/).join('').replace(',', '.'));
-
-        td = $table.find('td:contains("RUB")').next().next();
-        rubRate = +(td.text().split(/\s*/).join('').replace(',', '.'));
-
-        chrome.storage.sync.set({
-            usdRate: usdRate,
-            eurRate: eurRate,
-            rubRate: rubRate
-        }, function ()
+        else if (request.type == 'found')
         {
-        });
+
+            t = request.text.split(/\s*/).join('');
+
+            try
+            {
+                i = parseFloat(t);
+            } catch (ex) { };
+
+            if (i && !isNaN(i))
+            {
+                var title;
+
+                if (request.curr == 'usd')
+                {
+                    if (usdRate && !isNaN(usdRate))
+                    {
+                        title = '$' + numberToString(i / usdRate);
+                    }
+                }
+                else if (request.curr == 'eur')
+                {
+                    if (eurRate && !isNaN(eurRate))
+                    {
+                        title = '€' + numberToString(i / eurRate);
+                    }
+                }
+                else if (request.curr == 'rub')
+                {
+                    if (rubRate && !isNaN(rubRate))
+                    {
+                        title = '₽' + numberToString(i / rubRate);
+                    }
+                }
+
+                sendResponse({ text: title });
+
+            }
+        }
+        else if (request.type == 'rates')
+        {
+            sendResponse({
+                usdRate: usdRate,
+                eurRate: eurRate,
+                rubRate: rubRate,
+                byr: byr,
+                usd: usd,
+                eur: eur,
+                rub: rub
+            });
+        }
     });
-}
 
-take();
-setInterval(take, 5 * 60 * 1000);
 
+
+
+
+    function take()
+    {
+        var d = new Date();
+
+        $.ajax('http://www.nbrb.by/Services/XmlExRates.aspx?ondate=' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear())
+            .always(function (data)
+            {
+                var $data = $(data);
+
+                usdRate = +$(data).find('Currency[Id=145] Rate').text();
+                eurRate = +$(data).find('Currency[Id=19] Rate').text();
+                rubRate = +$(data).find('Currency[Id=190] Rate').text();
+
+                chrome.storage.sync.set({
+                    usdRate: usdRate,
+                    eurRate: eurRate,
+                    rubRate: rubRate
+                }, function ()
+                {
+                });
+            });
+    }
+
+
+    take();
+
+    var d = new Date();
+
+    chrome.alarms.create('takeRates', {
+        delayInMinutes: (23 - d.getHours()) * 60 + (60 - d.getMinutes()) + 15, // 15 мин после полуночи
+        periodInMinutes: 60 * 24
+    });
+
+    chrome.alarms.onAlarm.addListener(function (alarm)
+    {
+        if (alarm.name == 'takeRates')
+        {
+            take();
+        }
+    });
+
+})();
